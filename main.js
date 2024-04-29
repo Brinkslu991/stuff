bd5a51e4727698a84ab28233851bcdc9e244347f
Brinkslu991 <153300807+Brinkslu991@users.noreply.github.com>
2024-04-29 11:07:13 -0400

    zxcvsdfghiujhb

---
+++ main.js
@@ -0,0 +1,107 @@
+//Lucas Beinks
+//4/29/24
+//array EVIDENCE
+
+const firstName = 'Lucas';
+const lastName = 'Brinks';
+const school = 'Home School';
+const stuff = ['Dishwasher', 'Cracker', 'Pen', 'Dog', 'Hydrongen Bomb'];
+
+const length = stuff.length;
+
+const nameStr = (`Hello ${firstName} ${lastName} from ${school}`)
+const things = (`Your array is ${stuff}`)
+
+console.log('\n')
+console.log(nameStr)
+console.log(things)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+stuff.push('Graham Cracker');
+stuff.pop('Graham Cracker');
